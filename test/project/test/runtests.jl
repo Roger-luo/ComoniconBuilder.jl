a5ac6623d8c55a99b630cde8807b57892b442b27
Bonan Zhu <zhubonan@outlook.com>
2021-09-22 10:51:08 +0100

    Enable build test for the test project

--- test/project/test/runtests.jl
+++ test/project/test/runtests.jl
@@ -31,9 +31,49 @@ options = ComoniconOptions.Comonicon(
     install=ComoniconOptions.Install(
         path=usr_dir,
     ),
-    sysimg=ComoniconOptions.SysImg(;)
+    sysimg=ComoniconOptions.SysImg(precompile=ComoniconOptions.Precompile(["deps/precompile_execution.jl"], []))
 )
 
 # empty!(ARGS)
 # push!(ARGS, "sysimg")
 # install(TestProject, options)
+
+if get(ENV, "TEST_BUILD_SYSIMG", false) == "true"
+    @testset "System Image building" begin
+        options_sys = ComoniconOptions.Comonicon(
+            name="test",
+            install=ComoniconOptions.Install(
+                path=usr_dir,
+            ),
+        sysimg=ComoniconOptions.SysImg(precompile=ComoniconOptions.Precompile(["deps/precompile_execution.jl"], []))
+    )
+        outpath = ComoniconBuilder.pkgdir(TestProject, "deps/lib/libtest.so")
+        
+        isfile(outpath) && rm(outpath)
+        ComoniconBuilder.build_sysimg(TestProject, options_sys)
+        # Test if the new system image is present
+        @test isfile(outpath) && isnothing(rm(outpath))
+    end
+end
+
+if get(ENV, "TEST_BUILD_APP", false) == "true"
+    @testset "App building" begin
+        options_app = ComoniconOptions.Comonicon(
+            name="test",
+            install=ComoniconOptions.Install(
+                path=usr_dir,
+            ),
+        application=ComoniconOptions.Application(
+            precompile=ComoniconOptions.Precompile(["deps/precompile_execution.jl"], []),
+            incremental=true,
+            filter_stdlibs=false,
+            )
+    )
+        outpath = ComoniconBuilder.pkgdir(TestProject, "build/test/bin/test")
+
+        isfile(outpath) && rm(outpath)
+        ComoniconBuilder.build_application(TestProject, options_app)
+        # Test if the app has been built
+        @test isfile(outpath) && isnothing(rm(outpath))
+    end
+end
